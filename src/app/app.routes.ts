d2a0a80a5916cbbdcaecbc0ce4d55ea442b0bd8a
Wilmer Betancourt <wilmergabybet@gmail.com>
2025-02-26 17:12:41 -0500

    Creacion de entorno

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { EmpleadosComponent } from './pages/empleados/empleados.component';
+import { Error404Component } from './pages/error404/error404.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'home', component:HomeComponent},
+    {path: 'personal', component: EmpleadosComponent},
+
+    {path: '', redirectTo:'home', pathMatch:'full'},
+
+    {path: '**', component: Error404Component},
+    
+];
