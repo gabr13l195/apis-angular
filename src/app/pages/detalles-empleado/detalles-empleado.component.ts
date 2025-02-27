0117d539b19e2f93e0fe0e67a0c0f0ea079a669b
Wilmer Betancourt <wilmergabybet@gmail.com>
2025-02-27 17:44:12 -0500

    Cambios datos en bruto

--- src/app/pages/detalles-empleado/detalles-empleado.component.ts
+++ src/app/pages/detalles-empleado/detalles-empleado.component.ts
@@ -1,19 +1,30 @@
 import { Component, inject } from '@angular/core';
 import { EmpleadoService } from '../../services/empleado.service';
+import { ActivatedRoute } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-detalles-empleado',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './detalles-empleado.component.html',
   styleUrl: './detalles-empleado.component.css'
 })
 export class DetallesEmpleadoComponent {
 
   servicio = inject(EmpleadoService)
+  ruta = inject(ActivatedRoute)
+  id : any
+
+  empleado: any;
+
 
   ngOnInit(){
-    this.servicio.getUnicoEmpleado(3).subscribe(e=>{
+    this.ruta.params.subscribe(parametro =>{
+      this.id=parametro['idEmpleado']
+    })
+    this.servicio.getUnicoEmpleado(this.id).subscribe(e=>{
       console.log(e);
+      this.empleado=e
     })
   }
 
